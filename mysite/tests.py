e60135d385c6beab86bc3cde0104b7e94db0d847
Tony Lefebvre <tony.lefebvre.contractor@ardian.com>
2024-02-27 10:18:35 +0100

    facto

--- mysite/tests.py
+++ mysite/tests.py
@@ -3,19 +3,20 @@ import aiohttp
 import pytest
 
 
-@pytest.mark.asyncio
-async def test_async_ping(live_server):
-    url = f"{live_server.url}/ping"
+async def multi_calls(url):
     async with aiohttp.ClientSession() as session:
         resp_1, resp_2 = await asyncio.gather(session.get(url), session.get(url))
         assert resp_1.status == 200
         assert resp_2.status == 200
+
+
+@pytest.mark.asyncio
+async def test_async_ping(live_server):
+    url = f"{live_server.url}/ping"
+    await multi_calls(url)
 
 
 @pytest.mark.asyncio
 async def test_async_az(live_server):
     url = f"{live_server.url}/az"
-    async with aiohttp.ClientSession() as session:
-        resp_1, resp_2 = await asyncio.gather(session.get(url), session.get(url))
-        assert resp_1.status == 200
-        assert resp_2.status == 200
+    await multi_calls(url)
