5392d1fc78214a9c51a7d973bd881b32c8c11c4d
Tony Lefebvre <tony.lefebvre.contractor@ardian.com>
2024-02-27 10:18:42 +0100

    add never_cache

--- mysite/views.py
+++ mysite/views.py
@@ -1,12 +1,13 @@
 import logging
 from math import log
 from django.http import HttpResponse
-
+from django.views.decorators.cache import never_cache
 from .tasks import add
 
 logger = logging.getLogger(__name__)
 
 
+@never_cache
 def az(request):
     for x in range(1000):
         add.delay(x, x + 1)
